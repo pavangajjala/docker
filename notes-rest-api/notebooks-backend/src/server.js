47e167ac8eb5de5a35c2718350a6b7fec7bbb06d
Pavan Gajjala <pavankumar0177@gmail.com>
2025-03-25 14:10:34 +0000

    Added server js file for hello msg and listending port

--- notes-rest-api/notebooks-backend/src/server.js
+++ notes-rest-api/notebooks-backend/src/server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => res.json({message: 'Hello from notebooks'}));
 
-app.listen(3000, () => {
-    console.log('Notebooks server listening on port 3000')
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log('Notebooks server listening on port ${port}')
 });
